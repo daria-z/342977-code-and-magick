a36ffc20c3a566c6ad8d326b458b421ce61686d6
daria-z <daria.zakirova@gmail.com>
2018-04-17 15:30:24 +0300

    Реализовано перемещение окна и перетаскивание предмета

--- js/setup.js
+++ js/setup.js
@@ -87,6 +87,8 @@ var openPopup = function () {
 var closePopup = function () {
   setup.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
+  setup.style.top = '';
+  setup.style.left = '';
 };
 
 setupOpen.addEventListener('click', function () {
@@ -150,4 +152,90 @@ eyesColor.addEventListener('click', function () {
 
 fireballColor.addEventListener('click', function () {
   fireballColorChange();
+});
+
+
+var dialogHandle = setup.querySelector('.upload .setup-user-pic');
+
+dialogHandle.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    setup.style.top = (setup.offsetTop - shift.y) + 'px';
+    setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+var shopElement = document.querySelector('.setup-artifacts-shop .setup-artifacts-cell');
+var draggedItem = null;
+
+shopElement.addEventListener('dragstart', function (evt) {
+  if (evt.target.tagName.toLowerCase() === 'img') {
+    draggedItem = evt.target;
+    evt.dataTransfer.setData('text/plain', evt.target.alt);
+  }
+});
+
+var artifactsElement = document.querySelector('.setup-artifacts');
+var artifactsCell = document.querySelector('.setup-artifacts .setup-artifacts-cell:first-of-type');
+var shopCell = document.querySelector('.setup-artifacts-shop .setup-artifacts-cell:first-of-type');
+
+shopElement.addEventListener('dragstart', function () {
+  artifactsCell.style.outline = '2px dashed red';
+});
+
+artifactsElement.addEventListener('dragover', function (evt) {
+  evt.preventDefault();
+  return false;
+});
+
+artifactsElement.addEventListener('drop', function (evt) {
+  if (evt.target !== artifactsCell) {
+    shopCell.appendChild(draggedItem);
+  } else {
+    evt.target.appendChild(draggedItem);
+  }
+  evt.target.style.backgroundColor = '';
+  evt.target.style.outline = '';
+  evt.preventDefault();
+});
+
+
+artifactsElement.addEventListener('dragenter', function (evt) {
+  evt.target.style.backgroundColor = 'yellow';
+  evt.target.style.outline = '';
+  evt.preventDefault();
+});
+
+artifactsElement.addEventListener('dragleave', function (evt) {
+  evt.target.style.backgroundColor = '';
+  evt.target.style.outline = '';
+  evt.preventDefault();
 });
