c9c4c22b895321f96058f3c5aaa47850532dda28
daria-z <daria.zakirova@gmail.com>
2018-04-26 22:30:30 +0300

    Отправка корректных данных

--- js/backend.js
+++ js/backend.js
@@ -1,75 +1,55 @@
 'use strict';
 (function () {
+  var generateRequest = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      var error;
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+        case 400:
+          error = 'Неверный запрос';
+          break;
+        case 401:
+          error = 'Пользователь не авторизован';
+          break;
+        case 404:
+          error = 'Ничего не найдено';
+          break;
+        default:
+          error = 'Статус ответа: : ' + xhr.status + ' ' + xhr.statusText;
+      }
+      if (error) {
+        onError(error);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError(xhr.response);
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться');
+    });
+    return xhr;
+  };
 
   window.backend = {
     load: function (onLoad, onError) {
       var URL = 'https://js.dump.academy/code-and-magick/data';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      xhr.addEventListener('load', function () {
-        var error;
-        switch (xhr.status) {
-          case 200:
-            onLoad(xhr.response);
-            break;
-          case 400:
-            error = 'Неверный запрос';
-            break;
-          case 401:
-            error = 'Пользователь не авторизован';
-            break;
-          case 404:
-            error = 'Ничего не найдено';
-            break;
-          default:
-            error = 'Статус ответа: : ' + xhr.status + ' ' + xhr.statusText;
-        }
-        if (error) {
-          onError(error);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError(xhr.response);
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться');
-      });
+      var xhr = generateRequest(onLoad, onError);
       xhr.timeout = 10000;
       xhr.open('GET', URL);
       xhr.send();
+
     },
     save: function (data, onLoad, onError) {
       var URL = 'https://js.dump.academy/code-and-magick';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        var error;
-        switch (xhr.status) {
-          case 200:
-            onLoad(xhr.response);
-            break;
-          case 400:
-            error = 'Неверный запрос';
-            break;
-          case 401:
-            error = 'Пользователь не авторизован';
-            break;
-          case 404:
-            error = 'Ничего не найдено';
-            break;
-          default:
-            error = 'Статус ответа: : ' + xhr.status + ' ' + xhr.statusText;
-        }
-        if (error) {
-          onError(error);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError(xhr.response);
-      });
+      var xhr = generateRequest(onLoad, onError);
+      xhr.timeout = 10000;
       xhr.open('POST', URL);
       xhr.send(data);
+
     },
   };
 })();
--- js/setup.js
+++ js/setup.js
@@ -20,7 +20,7 @@
     }
   });
 
-  var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var WIZARD_EYES_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var FIERBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
   var eyesColor = window.util.setup.querySelector('.setup-wizard .wizard-eyes');
